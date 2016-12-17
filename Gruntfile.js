ee81a2d0eed3c675d8ef43b0247d34b2abad9354
aszymczyk <arekszymczyk15@gmail.com>
2016-12-17 17:13:45 +0100

    delete style.css

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
   		},
   		dist: {
   			files: {
-  				'css/style.css': 'css/style.sass'
+  				'css/style.css': 'main/style.sass'
   			}
   		}
   	},
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
   				expand: true,
   				cwd: 'images/',
   				src: ['**/*.{png,jpg,gif,jpeg,JPEG}'],
-  				dest: 'images/build/'
+  				dest: 'images/'
   			}]
   		}
   	}
